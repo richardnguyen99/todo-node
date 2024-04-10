af2c4e945327214f8dd757495e9aa18f2b0b1fbd
Richard Nguyen <richard@richardhnguyen.com>
2024-04-09 22:11:38 -0700

    feat: add .editorconfig to enforce general editting styles
    - .editorconfig added on the root directory
    - Updated existing files in the codebase

--- src/@types/process.d.ts
+++ src/@types/process.d.ts
@@ -1,16 +1,16 @@
 // Declare used key-value pairs in process.env
 
 declare namespace NodeJS {
-  interface ProcessEnv {
-    // Node Environment
-    NODE_ENV: "development" | "production";
-    PORT: string;
+    interface ProcessEnv {
+        // Node Environment
+        NODE_ENV: "development" | "production";
+        PORT: string;
 
-    // Database
-    DB_USER: string;
-    DB_PASSWORD: string;
-    DB_HOST: string;
-    DB_PORT: number;
-    DB_NAME: string;
-  }
+        // Database
+        DB_USER: string;
+        DB_PASSWORD: string;
+        DB_HOST: string;
+        DB_PORT: number;
+        DB_NAME: string;
+    }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
 
+/**
+ * A factory function that configurates Express application and return an
+ * Express instance.
+ *
+ * @returns {express.Application} app
+ */
 const createApp = () => {
-  const app = express();
+    const app = express();
 
-  app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+    app.get("/", (req, res) => {
+        res.send("Hello World");
+    });
 
-  return app;
+    return app;
 };
 
 export default createApp;
